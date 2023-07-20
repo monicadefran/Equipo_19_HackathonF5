2717eafae5f2a188641402efaba31d6fc34e9ae8
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-07-20 23:12:41 +0200

    Create conect to db

--- codexchange/src/Components/Navbar.jsx
+++ codexchange/src/Components/Navbar.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Navbar, Nav } from 'react-bootstrap';
-import '../Css/Navbar.css';
 import { FaUser } from 'react-icons/fa';
+import '../Css/Navbar.css';
 import nav from '../assets/nav.png';
 import logo from '../assets/logo.png';
 
 function NavScrollExample() {
-  const contactEmail = 'contacto@tudominio.com'; 
+  const [contactClicks, setContactClicks] = useState(0);
+  const contactEmail = 'contacto@tudominio.com';
+
+  const handleContactClick = () => {
+    setContactClicks(contactClicks + 1);
+  };
+
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -18,9 +24,15 @@ function NavScrollExample() {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-              <Nav.Link as={Link} to="/">Inicio</Nav.Link>
-              <Nav.Link as={Link} to="/Createads">Publicar</Nav.Link>
-              <Nav.Link href={`mailto:${contactEmail}`}>Contacto</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Inicio
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Createads">
+                Publicar
+              </Nav.Link>
+              <Nav.Link href={`mailto:${contactEmail}`} onClick={handleContactClick}>
+                Contacto
+              </Nav.Link>
             </Nav>
             <Nav style={{ marginRight: '50px' }}>
               <FaUser size={24} />
@@ -30,10 +42,13 @@ function NavScrollExample() {
       </Navbar>
       <div style={{ position: 'relative' }}>
         <img src={nav} height="100%" className="d-block align-top mx-auto" alt="Nav" />
-        <div className='textimage'>
-          Potencia tu proyecto con el banco de tiempo de desarrolladores:
-          intercambia habilidades, construye sinergias y haz realidad tus ideas con expertos tech voluntarios.
+        <div className="textimage">
+          Potencia tu proyecto con el banco de tiempo de desarrolladores: intercambia habilidades,
+          construye sinergias y haz realidad tus ideas con expertos tech voluntarios.
         </div>
+      </div>
+      <div style={{ marginTop: '20px', textAlign: 'center' }}>
+        <p>Hay  {contactClicks} nuevas notificaciones.</p>
       </div>
     </>
   );
