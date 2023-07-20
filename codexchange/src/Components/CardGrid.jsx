5f749b9463b9d996c7ba83e00843858ffe5d00e6
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-07-20 14:24:58 +0200

    Create Button Contact

--- codexchange/src/Components/CardGrid.jsx
+++ codexchange/src/Components/CardGrid.jsx
@@ -48,7 +48,7 @@ function GridExample() {
     <>
    
       <Container style={{ marginTop: '2rem' }}>
-        {/* Aquí está la barra de búsqueda */}
+       
         <Form style={{ marginBottom: '2rem' }}> 
           <Form.Group controlId="searchTerm">
             <Form.Control
@@ -65,8 +65,8 @@ function GridExample() {
           {filteredData.map((coder) => (
             <Col key={coder.id}>
               <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
+                <Card.Img variant="top" src="https://png.pngtree.com/png-vector/20191003/ourmid/pngtree-cyber-man-icon-isolated-on-abstract-background-png-image_1779361.jpg" style={{ width: '50%', height: 'auto',  margin: 'auto', marginTop: '5%'}}/>
+                <Card.Body className="text-center">
                   <Card.Title>{coder.Nombre}</Card.Title>
                   <Card.Text>
                     {coder.Posicion}
@@ -81,6 +81,7 @@ function GridExample() {
                   <Button variant="secondary" href={coder.GitHub}>
                     GitHub
                   </Button>
+                  <Contact />
                 </Card.Body>
               </Card>
             </Col>
